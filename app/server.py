81fb2d5b5e5c8704944641dc13bc6d663233ae29
reet-ii <65170734+reet-ii@users.noreply.github.com>
2020-05-13 14:59:27 +0530

    Update server.py
    classes changes and file path and name updated

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1-0MrQDuF2QcGCFkkkK2la-BWadAAJEf8'
+export_file_name = 'dance_resnet_50_9.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['ballet', 'belly dance', 'bhangra', 'bharatnatyam', 'break_dance', 'garba', 'jazz', 'kathak', 'kathakali', 'manipuri', 'salsa', 'waacking']
 path = Path(__file__).parent
 
 app = Starlette()
